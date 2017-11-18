3c4fa9487138c8f1dbd9c0148b5f7d56303293ad
Mohamad Mawaldi <ima.mawaldi@gmail.com>
2017-11-18 17:34:25 +0100

    no message

--- app/src/main/java/com/example/hammode/todolist/MainActivity.java
+++ app/src/main/java/com/example/hammode/todolist/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,12 +19,11 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import static android.view.View.*;
 
 public class MainActivity extends dbhandler {
-    //EditText titlein ;
-    //EditText descriptionin ;
+
    private ListView listo ;
    private FloatingActionButton fab ;
    private CustomAdapter adapter ;
-   private EditText title,description;
+   private EditText title,description,search;
    private Button done,cancel;
    private TextView head;
 
@@ -30,14 +31,30 @@ public class MainActivity extends dbhandler {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-         //titlein = (EditText) findViewById(R.id.titlein);
-        //descriptionin = (EditText)findViewById(R.id.descriptionin);
          adapter = new CustomAdapter(MainActivity.this);
          listo = (ListView)findViewById(R.id.listview);
          fab= (FloatingActionButton)findViewById(R.id.fab);
           db = FirebaseFirestore.getInstance();
         adapter = new CustomAdapter(MainActivity.this);
+        search = (EditText)findViewById ( R.id.search );
+        search.addTextChangedListener ( new TextWatcher () {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                (MainActivity.this).adapter.getFilter ().filter ( s );
+                adapter.notifyDataSetChanged ();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        } );
         listo.setAdapter(adapter);
         adapter.notifyDataSetChanged();
         Loader();
@@ -71,24 +88,7 @@ public class MainActivity extends dbhandler {
                     public void onClick(View v) {
                         dialog.dismiss ();
                     }
-                } );
-
-
-
-                /*Dialog dialog = new Dialog ( MainActivity.this );
-                dialog.setContentView ( R.layout.dialog );
-                dialog.show ();
-                title = (EditText)dialog.findViewById(R.id.title);
-                description = (EditText)dialog.findViewById(R.id.description);
-                head = (TextView )dialog.findViewById(R.id.head);
-                done = (Button )dialog.findViewById(R.id.done);
-                cancel = (Button )dialog.findViewById(R.id.cancel);*/
-
-                //add(titlein.getText().toString() ,descriptionin.getText().toString());
-                //Loader();
-                //titlein.setText("");
-                //descriptionin.setText("");
-                }});
+                } );}});
     }
 
     @Override
@@ -109,8 +109,8 @@ public class MainActivity extends dbhandler {
             dialog.show ();
             title = ( EditText ) dialog.findViewById ( R.id.title );
             description = ( EditText ) dialog.findViewById ( R.id.description );
-            title.setText ( SharedList.list.get ( item.getOrder () ).getTitle () );
-            description.setText ( SharedList.list.get ( item.getOrder () ).getDescription () );
+            //title.setText ( SharedList.list.get ( item.getOrder () ).getTitle () );
+            //description.setText ( SharedList.list.get ( item.getOrder () ).getDescription () );
             head = ( TextView ) dialog.findViewById ( R.id.head );
             head.setText ( "Update!" );
             done = ( Button ) dialog.findViewById ( R.id.done );
